44fc4b0bbb7dda47da2dcdfd3bed4aa512088686
Romário Lima <devromariolima@gmail.com>
2024-05-20 18:26:58 -0300

    Style: Ajustes na responsividade

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Função que gera um objeto com dados aleatórios e exibe na tela
- function gerarEDisplayDados() {
+function gerarEDisplayDados() {
   const temperatura = Math.round(Math.random() * 100);
   const watt = Math.round(Math.random() * 100);
   const pressao = Math.round(Math.random() * 100);
@@ -9,18 +9,17 @@
     watt: watt,
     pressao: pressao,
 
-};  console.log(dados);
+  }; console.log(dados);
 
-  if(temperatura >= 80){
-           toggleDiv();
-          console.log(toggleDiv + "chamou função")
-        }
+  if (temperatura >= 80) {
+    toggleDiv();
+  }
 
   // Atualiza os elementos HTML com os dados gerados
   document.getElementById('p_temp').textContent = temperatura + " °C";
   document.getElementById('p_Watt').textContent = watt + " Watts";
-  document.getElementById('p_press').textContent = pressao  + " Psi";
-   
+  document.getElementById('p_press').textContent = pressao + " Psi";
+
 
   // Chama a função novamente após 2 segundos
   setTimeout(gerarEDisplayDados, 3000);
@@ -29,17 +28,17 @@
 // Iniciar a geração de dados imediatamente
 gerarEDisplayDados();
 
-function toggleDiv ()  {
-var show = document.getElementById("alert");
-show.style.display = "block";
+function toggleDiv() {
+  var show = document.getElementById("alert");
+  show.style.display = "block";
 
-  }
+}
 
-  function hideDiv ()  {
-    var hide = document.getElementById("alert");
-    hide.style.display = "none";
-  
-      }
+function hideDiv() {
+  var hide = document.getElementById("alert");
+  hide.style.display = "none";
 
-      setInterval(hideDiv,  5000);
+}
+
+setInterval(hideDiv, 5000);
 
